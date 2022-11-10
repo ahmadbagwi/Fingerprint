81398aa4c868d287a7a831f3c570707353e0d626
ahmad-x <ahmadbagwi.id@gmail.com>
2022-11-10 09:30:41 +0700

    fix use dotenv

--- Fingerprint.php
+++ Fingerprint.php
@@ -1,23 +1,34 @@
 <?php
-
+require_once realpath(__DIR__ . '/vendor/autoload.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require('ZKLibrary.php');
 
 class Fingerprint {
 	function __construct ()
     {
-        $this->ipmesin = getenv('IP');
-        $this->portmesin = getenv('PORT');
+        $this->dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $this->dotenv->safeLoad();
+        $this->ip = $_ENV['IP'];
+        $this->port = $_ENV['PORT'];
+    }
+
+    function test () {
+        $test = ['Lorem', 'Ipsum'];
+        header("Content-Type: application/json");
+        return json_encode($test);
     }
 
     function all_attendance ()
     {
-        $zk = new ZKLibrary($this->ipmesin, $this->portmesin);
+        $zk = new ZKLibrary($this->ip, $this->port);
         $zk->connect();
         $zk->disableDevice();
         $data = $zk->getAttendance();
         $zk->enableDevice();
         $zk->disconnect();
-
+        
 		header("Content-Type: application/json");
 		return json_encode($data);
     }

7fe7accf43fde9a275dde0019d7a9773d562be03
ahmad-x <ahmadbagwi.id@gmail.com>
2022-11-22 19:05:52 +0700

    Test store & update

--- Fingerprint.php
+++ Fingerprint.php
@@ -18,6 +18,7 @@ class Fingerprint {
         $this->dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
         $this->dotenv->safeLoad();
         $this->ip = $_ENV['IP'];
+        $this->ip2 = $_ENV['IP2'];
         $this->port = $_ENV['PORT'];
         $this->base_url = $_ENV['BASE_URL'];
         // $this->late = $this->http_request($this->base_url.'/'.'api/fingerprint/terlambat';
@@ -52,9 +53,9 @@ class Fingerprint {
         return $response;
     }
 
-    function all_attendance ()
+    function all_attendance ($ip)
     {
-        $zk = new ZKLibrary($this->ip, $this->port);
+        $zk = new ZKLibrary($ip, $this->port);
         $zk->connect();
         $zk->disableDevice();
         $get_attendance = $zk->getAttendance();
@@ -63,9 +64,9 @@ class Fingerprint {
 		return $get_attendance;
     }
 
-    function today_attendance ($date)
+    function today_attendance ($date, $ip)
     {
-        $all_attendance = $this->all_attendance();
+        $all_attendance = $this->all_attendance($ip);
         $today_attendance = [];
         $late = $this->http_request($this->base_url.'/'.'api/fingerprint/terlambat');
         $status = '';
@@ -110,10 +111,63 @@ class Fingerprint {
         return $today_attendance_fix;
     }
 
-    function store_attendance ($date)
+    function today_update_attendance ($date, $ip)
+    {
+        $all_attendance = $this->all_attendance($ip);
+        $today_update_attendance = [];
+
+        foreach ($all_attendance as $all_data) {
+            $time = explode(" ", $all_data[3]);
+            // get today only
+            if ($time[0] == $date) {
+                // push all today data to today_attendance
+                array_push($today_update_attendance, [
+                    'user_id' => $this->http_request($this->base_url.'/'.'api/fingerprint/profil/id-mesin/'.$all_data[1]),
+                    'id_mesin' => $all_data[1],
+                    'tanggal' => $time[0],
+                    'pulang' => $time[1]
+                ]);
+            }
+        }
+
+        // get today data from database
+        // get only data already in database to update 
+        $today_attendance_database = json_encode($this->http_request($this->base_url.'/'.'api/fingerprint/absen/'.$date));
+        $today_attendance_compare = [];
+
+        foreach (array_reverse($today_update_attendance) as $all_today) {
+            $exist = false;
+            foreach (array_reverse($today_attendance_database) as $all_database) {
+                if ($all_today['user_id'] == $all_database['user_id']
+                    && $all_today['id_mesin'] == $all_database['id_mesin']
+                    && $all_today['tanggal'] == $all_database['tanggal']){
+                    $exist = true;
+                }
+            }
+            if ($exist === true) array_push($today_attendance_compare, $all_today);
+        }
+
+        // remove duplicate if touch fingerprint more than once, by id_mesin
+        // https://stackoverflow.com/questions/307674/how-to-remove-duplicate-values-from-a-multi-dimensional-array-in-php
+        $temp = array_unique(array_column($today_attendance_compare, 'id_mesin'));
+        $today_attendance_update_fix = array_intersect_key($today_attendance_compare, $temp);
+        return $today_attendance_update_fix;
+    }
+
+    function store_attendance ($date, $ip)
     {
         $url = $this->http_request($this->base_url.'/'.'api/fingerprint/attendance/store');
-        $data = $this->today_attendance($date);
+        $data = $this->today_attendance($date, $ip);
+        $username = $_ENV['USERNAME'];
+        $password = $_ENV['PASSWORD'];
+        $submit = $this->http_post($url, $data, $username, $password);
+        return $submit;
+    }
+
+    function update_attendance ($date, $ip)
+    {
+        $url = $this->http_request($this->base_url.'/'.'api/fingerprint/attendance/update');
+        $data = $this->today_update_attendance($date, $ip);
         $username = $_ENV['USERNAME'];
         $password = $_ENV['PASSWORD'];
         $submit = $this->http_post($url, $data, $username, $password);
